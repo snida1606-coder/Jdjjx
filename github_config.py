3536106c8ae5d5eb8ced48624ea580db5420595a
snida1606-coder <snida1606@gmail.com>
2026-06-22 00:09:20 +0500

    Update github_config.py

--- github_config.py
+++ github_config.py
@@ -1,5 +1,7 @@
-GITHUB_USER = "ghulammujtabaquotex-cloud"
-GITHUB_REPO = "Quotex-server"
-GITHUB_TOKEN = "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"
+import os
+
+GITHUB_USER = os.environ.get("GITHUB_USER", "ghulammujtabaquotex-cloud")
+GITHUB_REPO = os.environ.get("GITHUB_REPO", "Quotex-server")
+GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN", "")
 GITHUB_RAW = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/main"
 
