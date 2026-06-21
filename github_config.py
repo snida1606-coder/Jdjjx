3e6a18b56902f6fe1ff85cc574ff2095be1fb754
snida1606-coder <snida1606@gmail.com>
2026-06-22 00:08:27 +0500

    Add files via upload

---
+++ github_config.py
@@ -0,0 +1,5 @@
+GITHUB_USER = "ghulammujtabaquotex-cloud"
+GITHUB_REPO = "Quotex-server"
+GITHUB_TOKEN = "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"
+GITHUB_RAW = f"https://raw.githubusercontent.com/{GITHUB_USER}/{GITHUB_REPO}/main"
+
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install -r requirements.txt && python3 server_history.py
